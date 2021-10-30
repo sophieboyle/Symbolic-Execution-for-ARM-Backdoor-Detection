44f6b7d5998fde4de69d51819049bd86ba685245
Sophie Boyle <sophieboyle4028@gmail.com>
2021-10-31 00:04:48 +0100

    Moved code into class

--- src/main.py
+++ src/main.py
@@ -1,11 +1,75 @@
 import angr
 import argparse
-import sys
+
+
+class Analyser:
+    def __init__(self, filename, authentication_identifiers):
+        """
+        string filename : The name of the binary to be analysed
+        dict authentication_identifiers : dictionary with each key stating the type of\
+                                        data which is considered an 'authentication\
+                                        identifier'. Each key can be mapped to a list\
+                                        of possible identifiers
+        """
+        self.filename = filename
+        self.authentication_identifiers = authentication_identifiers
+        self.project = project = angr.Project(filename)
+        self.entry_state = project.factory.entry_state()
+        print(self.authentication_identifiers["string"][0])
+
+    def find_paths_to_auth_strings(self, sim, auth_strings):
+        for auth_str in auth_strings:
+            sim.explore(find=lambda s: bytes(auth_str, 'utf-8') in s.posix.dumps(1),
+                    avoid=lambda s: b'Access denied' in s.posix.dumps(1))
+            if sim.found:
+                access_state = sim.found[0]
+                print(f"Credentials for access string {auth_str}:\
+                        {self.parse_solution_dump(access_state.posix.dumps(0))}")
+            else:
+                print("No solution")
+
+    def run_symbolic_execution(self):
+        sim = self.project.factory.simgr(self.entry_state)
+        self.find_paths_to_auth_strings(sim, self.authentication_identifiers["string"])
+
+    def parse_solution_dump(self, bytestring):
+        """
+        A solution must be parsed since angr cannot work with fgets or scanf.
+        This is because it cannot handle the dynamic number of values readable
+        from scanf or fgets. It applies constraints based on the full size of 
+        the buffer instead.
+
+        This means that the normal results returned by access_state.posix.dumps(0))
+        may be filled with garbage bytes after the null character. This function
+        is simply a way of formatting the output such that the garbage bytes
+        are not printed.
+
+        This could result in an extra output being emitted as an error, since a
+        garbage byte may by chance be an alphanumeric character.
+
+        Run tool at least twice to identify undefined behaviour.
+
+        TODO: Fix this by hooking fgets() functionality in angr?
+        """
+        results = []
+        tmp = ''
+        print(bytestring)
+        iterbytes = [bytestring[i:i+1] for i in range(len(bytestring))]
+        for b in iterbytes:
+            if b == b'\x00':
+                results.append(tmp)
+                tmp = ''
+                continue
+            try:
+                tmp += b.decode('utf-8')
+            except:
+                pass
+        return results
 
 
 def arg_parsing():
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', nargs=1, 
+    parser.add_argument('filename', nargs=1,
                         help='Filename of firmware to analyse')
     args = parser.parse_args()
     return args.filename[0]
@@ -17,70 +81,10 @@ def read_bytes(filename):
     return bytes_str
 
 
-def access_granted(state):
-    output_msg = state.posix.dumps(sys.stdout.fileno())
-    if b'Access granted' in output_msg:
-        return True
-    else:
-        return False
-
-
-def abort(state):
-    output_msg = state.posix.dumps(sys.stdout.fileno())
-    if b'Access denied' in output_msg:
-        return True
-    else:
-        return False
-
-
-def parse_solution_dump(bytestring):
-    """
-    A solution must be parsed since angr cannot work with fgets or scanf.
-    This is because it cannot handle the dynamic number of values readable
-    from scanf or fgets. It applies constraints based on the full size of 
-    the buffer instead.
-
-    This means that the normal results returned by access_state.posix.dumps(0))
-    may be filled with garbage bytes after the null character. This function
-    is simply a way of formatting the output such that the garbage bytes
-    are not printed.
-
-    This could result in an extra output being emitted as an error, since a
-    garbage byte may by chance be an alphanumeric character.
-
-    Run tool at least twice to identify undefined behaviour.
-
-    TODO: Fix this by hooking fgets() functionality in angr?
-    """
-    results = []
-    tmp = ''
-    print(bytestring)
-    iterbytes = [bytestring[i:i+1] for i in range(len(bytestring))]
-    for b in iterbytes:
-        if b == b'\x00':
-            results.append(tmp)
-            tmp = ''
-            continue
-        try:
-            tmp += b.decode('utf-8')
-        except:
-            pass
-    return results
-
-
 def main():
     filename = arg_parsing()
-    project = angr.Project(filename)
-
-    entry_state = project.factory.entry_state()
-    sim = project.factory.simgr(entry_state)
-    sim.explore(find=access_granted, avoid=abort)
-
-    if sim.found:
-        access_state = sim.found[0]
-        print(f"Solution found: {parse_solution_dump(access_state.posix.dumps(0))}")
-    else:
-        print("No solution")
+    analyser = Analyser(filename, {"string": ["Access granted"]})
+    analyser.run_symbolic_execution()
 
 
 if __name__ == '__main__':
