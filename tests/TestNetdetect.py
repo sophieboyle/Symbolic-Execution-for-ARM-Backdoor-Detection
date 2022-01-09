caf28da0f5a9cea58e1c091affef49774547499d
Sophie Boyle <sophieboyle4028@gmail.com>
2022-01-09 22:16:39 +0000

    Test for UDP Server

--- tests/TestNetdetect.py
+++ tests/TestNetdetect.py
@@ -122,5 +122,51 @@ class TestNetworkDetectionUdpClient(unittest.TestCase):
         self.assertEqual(self.network_table[self.addr]['recvfrom'][0][0], self.protocol)
 
 
+class TestNetworkDetectionUdpServer(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        analyser = Analyser("code-samples/networking/udp-server-sample",
+                                      {"string": [],
+                                       "file_operation": {"fread": [],
+                                                          "fwrite": [],
+                                                          "fopen": []},
+                                       "allowed_listening_ports": [],
+                                       "allowed_outbound_ports": []
+                                       },
+                            None
+                            )
+        cls.results = analyser.run_symbolic_execution()
+        cls.network_table = cls.results["network_table"]
+        cls.addr = ('0.0.0.0', 8888)
+        cls.addr2 = (None, None)
+        cls.protocol = 'UDP (SOCK_DGRAM)'
+
+    def test_address_is_detected(self):
+        if len(self.network_table.keys()) != 2:
+            self.fail("Addresses detected incorrectly")
+        else:
+            self.assertEqual([k for k, v in self.network_table.items()][0], self.addr)
+            self.assertEqual([k for k, v in self.network_table.items()][1], self.addr2)
+
+    def test_correct_call_numbers(self):
+        self.assertEqual(len(self.network_table[self.addr]['bind']), 1)
+        self.assertEqual(len(self.network_table[self.addr]['connect']), 0)
+        self.assertEqual(len(self.network_table[self.addr]['send']), 0)
+        self.assertEqual(len(self.network_table[self.addr]['recv']), 0)
+        self.assertEqual(len(self.network_table[self.addr]['sendto']), 0)
+        self.assertEqual(len(self.network_table[self.addr]['recvfrom']), 0)
+
+        self.assertEqual(len(self.network_table[self.addr2]['bind']), 0)
+        self.assertEqual(len(self.network_table[self.addr2]['connect']), 0)
+        self.assertEqual(len(self.network_table[self.addr2]['send']), 0)
+        self.assertEqual(len(self.network_table[self.addr2]['recv']), 0)
+        self.assertEqual(len(self.network_table[self.addr2]['sendto']), 1)
+        self.assertEqual(len(self.network_table[self.addr2]['recvfrom']), 1)
+
+    def test_protocol(self):
+        self.assertEqual(self.network_table[self.addr2]['sendto'][0][0], self.protocol)
+        self.assertEqual(self.network_table[self.addr2]['recvfrom'][0][0], self.protocol)
+
+
 if __name__ == '__main__':
     unittest.main()
