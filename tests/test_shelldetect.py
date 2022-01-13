695b2942e878439e8685afa77dccde7d649595c3
Sophie Boyle <sophieboyle4028@gmail.com>
2022-01-13 19:18:34 +0000

    Added test to ensure no false positive

--- tests/test_shelldetect.py
+++ tests/test_shelldetect.py
@@ -6,7 +6,8 @@ from parameterized import parameterized_class
 @parameterized_class([
     {"code_sample": "hidden-shell", "shell_strings_expected": ["/bin/sh"]},
     {"code_sample": "two-shell", "shell_strings_expected": ["/bin/sh", "/bin/ksh"]},
-    {"code_sample": "shell", "shell_strings_expected": ["/bin/sh"]}
+    {"code_sample": "shell", "shell_strings_expected": ["/bin/sh"]},
+    {"code_sample": "no-shell", "shell_strings_expected": []},
 ])
 class TestShellDetection(unittest.TestCase):
     @classmethod
