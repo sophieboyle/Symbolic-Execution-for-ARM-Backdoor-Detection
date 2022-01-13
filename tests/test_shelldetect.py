ece98f909491ca0a71010e4b17409bf80cd06012
Sophie Boyle <sophieboyle4028@gmail.com>
2022-01-13 19:11:22 +0000

    Created parameterised unittest class for testing shelldetect module

---
+++ tests/test_shelldetect.py
@@ -0,0 +1,32 @@
+import unittest
+from src.main import *
+from parameterized import parameterized_class
+
+
+@parameterized_class([
+    {"code_sample": "hidden-shell", "shell_strings_expected": ["/bin/sh"]},
+])
+class TestShellDetection(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        analyser = Analyser(f"code-samples/shell-strings/{cls.code_sample}",
+                                      {"string": [],
+                                       "file_operation": {"fread": [],
+                                                          "fwrite": [],
+                                                          "fopen": []},
+                                       },
+                            None
+                            )
+        cls.results = analyser.run_symbolic_execution()
+        cls.shell_strings = cls.results["shell_strings"]
+
+    def test_shell_strings_found(self):
+        if len(self.shell_strings_expected) != len(self.shell_strings):
+            self.fail("Shell strings detected incorrectly")
+        else:
+            self.assertEqual(self.shell_strings_expected.sort(),
+                             self.shell_strings.sort())
+
+
+if __name__ == '__main__':
+    unittest.main()
