0aaf9d3fc4b9bda9fa811c5354d167c3db526e1b
Sophie Boyle <sophieboyle4028@gmail.com>
2022-03-21 15:20:02 +0000

    Docstrings for main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,4 @@
-from logging import raiseExceptions
-import angr
 import argparse
-import socket
-import struct
-import re
 from src.modules.netdetect import *
 from src.modules.shelldetect import *
 from src.modules.filedetect import *
@@ -12,11 +7,13 @@ from src.modules.filedetect import *
 class Analyser:
     def __init__(self, filename, authentication_identifiers, output_file):
         """
-        string filename : The name of the binary to be analysed
-        dict authentication_identifiers : dictionary with each key stating the type of\
+        Initialises the analyser which runs all simulations
+        :param string filename : The name of the binary to be analysed
+        :param dict authentication_identifiers : dictionary with each key stating the type of\
                                         data which is considered an 'authentication\
                                         identifier'. Each key can be mapped to a list\
                                         of possible identifiers
+        :param output_file : Name of the file to output results to
         """
         self.filename = filename
         self.output_file = output_file
@@ -33,11 +30,14 @@ class Analyser:
     def find_func_addr(self, func_name):
         # TODO: Double check that returning predeccors[0] isn't going to cause problems...
         """
-        io_funct_name : string name of function to identify
-        file_accessed : the filename of the file that the function operates on
         Function that finds the address of IO to a given file. This will only work
         for binaries that haven't been stripped. Stripped binaries will require
         something like IDA's Fast Library Identification and Recognition Technology
+        :param io_funct_name : string name of function to identify
+        :param file_accessed : the filename of the file that the function operates on
+        :return: a list of addresses at which a function is called
+        :return: a list of predecessor blocks which occur before the calling block of
+        the given function
         """
         nodes = [n for n in self.cfg.nodes() if n.name == func_name]
         call_addresses = [n.predecessors[0].predecessors[0].addr for n in
@@ -46,12 +46,26 @@ class Analyser:
         return call_addresses, func_prelude_block_nodes
 
     def find_post_blocks(self, func_name):
+        """
+        Finds the block after a given function. This block is important to ensure that
+        it's possible to jump to the beginning of a block *after* a return value has been
+        loaded into a register
+        :param func_name: The function of which is the predecessor to the post block
+        :return: The list of post blocks which follow the blocks which call the given
+        function
+        """
         # TODO: Double check that returning predeccors[0] isn't going to cause problems...
         # TODO: Find the block after a function (e.g. socket) is executed, so that we can get the return arg
         nodes = [n for n in self.cfg.nodes() if n.name == func_name]
         return [n.successors[0].block for n in nodes]
 
     def find_paths_to_auth_strings(self, sim, auth_strings):
+        """
+        Unused function
+        :param sim:
+        :param auth_strings:
+        :return:
+        """
         for auth_str in auth_strings:
             sim.explore(find=lambda s: bytes(auth_str, 'utf-8') in s.posix.dumps(1),
                         avoid=lambda s: b'Access denied' in s.posix.dumps(1))
@@ -63,10 +77,21 @@ class Analyser:
                 print("No solution")
 
     def write_results_to_file(self):
+        """
+        Write output of analysis to file
+        :return: None
+        """
         with open(self.output_file, "w") as f:
             f.write(self.output_string)
 
     def get_addresses_and_blocks_for_func_names(self, func_names):
+        """
+        Gets the addresses, prelude blocks, and post blocks for all given function
+        names
+        :param func_names: List of strings (function names)
+        :return: Dictionary of addresses formatted {function: [addresses]}
+        :return: Dictionary of prelude blocks formatted {function: [blocks]}
+        """
         addresses = {}
         prelude_blocks = {}
         for func in func_names:
@@ -76,40 +101,25 @@ class Analyser:
         return addresses, prelude_blocks
 
     def run_symbolic_execution(self):
+        """
+        Run the symbolic execution analysis for the given binary
+        :return: A dictionary of the results in the format {detection_type: detecton_results}
+        """
         sim = self.project.factory.simgr(self.entry_state)
 
-        # file_io_addresses = {"fopen": self.find_func_addr("fopen"),
-        #                      "fwrite": self.find_func_addr("fwrite"),
-        #                      "fread": self.find_func_addr("fread"),
-        #                      "fscanf": self.find_func_addr("__isoc99_fscanf"),
-        #                      }
-
+        # Run file detection
         file_io_addresses, file_io_prelude_blocks = self.get_addresses_and_blocks_for_func_names(
                                                         ["fopen", "fwrite", "fread", "__isoc99_fscanf"])
-
         file_access_driver = FileAccessDriver(self.project, self.entry_state, file_io_addresses, file_io_prelude_blocks)
         self.results["file_access_table"] = file_access_driver.run_file_detection()
         self.output_string += file_access_driver.get_output_string()
 
-        if self.authentication_identifiers["string"]:
-            for auth_str in self.authentication_identifiers["string"]:
-                self.find_paths_to_auth_strings(sim, self.authentication_identifiers["string"])
-
         # Run network detection
-        # net_addresses = {"socket": self.find_func_addr('socket'),
-        #                  "accept": self.find_func_addr('accept'),
-        #                  "bind": self.find_func_addr('bind'),
-        #                  "connect": self.find_func_addr("connect"),
-        #                  "send": self.find_func_addr("send"),
-        #                  "sendto": self.find_func_addr("sendto"),
-        #                  "recvfrom": self.find_func_addr("recvfrom"),
-        #                  "recv": self.find_func_addr("recv")}
         socket_post_blocks = self.find_post_blocks("socket")
         net_addresses, net_prelude_blocks = self.get_addresses_and_blocks_for_func_names(
                                                         ["socket", "accept", "bind",
                                                          "connect", "send", "sendto",
                                                          "recvfrom", "recv"])
-
         net_driver = NetworkDriver(self.project, self.entry_state, net_addresses, net_prelude_blocks, socket_post_blocks)
         self.results["network_table"] = net_driver.run_network_detection()
         self.output_string += net_driver.get_output_string()
@@ -119,9 +129,9 @@ class Analyser:
         self.results["shell_strings"] = shellcmd_detect.find()
         self.output_string += shellcmd_detect.get_output_string()
 
+        # Handle output
         if self.output_file:
             self.write_results_to_file()
-
         print(self.output_string)
 
         return self.results
