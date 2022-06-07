9c7a766b9abb132e141ef06164d8af583f36044d
Sophie Boyle <sophieboyle4028@gmail.com>
2022-06-07 21:57:11 +0100

    Added test to check that the correct number of calls are detected for each network function

--- tests/test_netdetect_v2.py
+++ tests/test_netdetect_v2.py
@@ -8,7 +8,8 @@ from parameterized import parameterized_class
     {"code_sample": "tcp-client-sample",
         "comms": {('127.0.0.1', 8888): {"protocol": 1,
                                         "functions":
-                                            {"connect": {"number": 1},
+                                            {
+                                             "connect": {"number": 1},
                                              "send": {"number": 1, "size": None},
                                              "recv": {"number": 1, "size": None},
                                             }
@@ -34,6 +35,9 @@ class TestNetworkDetection(unittest.TestCase):
                 # Check if the protocol is correct
                 self.assertEqual(netfunctree.protocol, self.comms[addr]["protocol"])
                 # Check detected network functions are correct
+                for net_function, number_of_calls in netfunctree.func_dict.items():
+                    if number_of_calls != 0:
+                        self.assertEqual(self.comms[addr]["functions"][net_function]["number"], number_of_calls)
 
 
 if __name__ == '__main__':
