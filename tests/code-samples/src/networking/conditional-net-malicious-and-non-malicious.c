e30efb8855ea6bb28e6fbe7241cc0200732f728f
Sophie Boyle <sophieboyle4028@gmail.com>
2022-07-12 22:37:42 +0100

    Added networking test

---
+++ tests/code-samples/src/networking/conditional-net-malicious-and-non-malicious.c
@@ -0,0 +1,36 @@
+/*
+	C ECHO client example using sockets
+*/
+#include <stdio.h>	//printf
+#include <string.h>	//strlen
+#include <sys/socket.h>	//socket
+#include <arpa/inet.h>	//inet_addr
+#include <unistd.h>
+
+int main(int argc , char *argv[])
+{
+	int sock;
+	struct sockaddr_in server;
+	char server_reply[2000];
+	
+    char message[1000] = "hello";
+
+	//Create socket
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	server.sin_family = AF_INET;
+	
+    int b = 0;
+    scanf("%d", &b);
+
+    sock = socket(AF_INET , SOCK_STREAM , 0);
+
+    if (b){
+        server.sin_port = htons( 8080 );
+        bind(sock , (struct sockaddr*)&server, sizeof(server) );
+    } else {
+        server.sin_port = htons( 1337 );
+        bind(sock , (struct sockaddr*)&server, sizeof(server) );
+    }
+    close(sock);
+	return 0;
+}
